a3d99b619c0dfd2e02c0c44569793fcc04812fa8
Caesar Richard Pitoy <caesar.pitoy@pegadaian.co.id>
2019-11-05 13:45:25 +0700

    GitTest v.6

--- src/Assignment/GitTest.java
+++ src/Assignment/GitTest.java
@@ -3,9 +3,10 @@ package Assignment;
 public class GitTest {
 
 	public static void main(String[] args) {
-		System.out.println("This is version 4");
+		System.out.println("This is version 6");
 		System.out.println("another one");
 		System.out.println("another two");
+		System.out.println("another three");
 	}
 
 }
