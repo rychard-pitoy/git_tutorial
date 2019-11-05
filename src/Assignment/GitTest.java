be0023bea864c5563b75bf50359d7b649f66ecbe
Caesar Richard Pitoy <caesar.pitoy@pegadaian.co.id>
2019-11-05 10:43:11 +0700

    GitTest v.3
    replace one line

--- src/Assignment/GitTest.java
+++ src/Assignment/GitTest.java
@@ -4,7 +4,7 @@ public class GitTest {
 
 	public static void main(String[] args) {
 		System.out.println("This is version 2");
-		System.out.println("Yeyyyy");
+		System.out.println("+1 Kidding hahaha");
 	}
 
 }
