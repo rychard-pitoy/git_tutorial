992271e6eb14b6ed04d807ae5ff6c0f80644f11a
Caesar Richard Pitoy <caesar.pitoy@pegadaian.co.id>
2019-11-05 10:14:21 +0700

    Update GitTest.java

--- src/Assignment/GitTest.java
+++ src/Assignment/GitTest.java
@@ -3,7 +3,8 @@ package Assignment;
 public class GitTest {
 
 	public static void main(String[] args) {
-		System.out.println("This is version 1");
+		System.out.println("This is version 2");
+		System.out.println("Yeyyyy");
 	}
 
 }
